b03b7e9a09f009f311d0b05fb0998a0d0fe66c08
Marco Lam <lamma3@oocl.com>
2020-04-21 15:01:12 +0800

    add Counter incrementByOne function

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -4,15 +4,23 @@ export class Counter extends Component {
   constructor(props) {
     super(props);
 
+    this.incrementByOne = this.incrementByOne.bind(this);
+
     this.state = {
       number: 0,
     };
   }
 
+  incrementByOne() {
+    this.setState(state => ({
+      number: state.number + 1,
+    }));
+  }
+
   render() {
     return (
       <div>
-        <button>+</button>
+        <button onClick={this.incrementByOne}>+</button>
         <span>{this.state.number}</span>
         <button>-</button>
       </div>
