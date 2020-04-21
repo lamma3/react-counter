9f38d68140c13a49b4f2b9b3bffdc12f210faf1c
Marco Lam <lamma3@oocl.com>
2020-04-21 16:38:11 +0800

    add input for counter group size

--- src/components/CounterGroup.js
+++ src/components/CounterGroup.js
@@ -5,14 +5,31 @@ export class CounterGroup extends Component {
   constructor(props) {
     super(props);
 
+    this.updateGroupSize = this.updateGroupSize.bind(this);
+
     this.state = {
       size: 2,
     };
   }
 
+  updateGroupSize(event) {
+    let newSize = parseInt(event.target.value);
+    this.setState((state) => ({
+      size: isNaN(newSize) ? 0 : newSize,
+    }));
+  }
+
   render() {
     return (
       <div>
+        <form>
+          <input
+            type="text"
+            value={this.state.size}
+            onChange={this.updateGroupSize}
+          ></input>
+        </form>
+
         {Array.from(Array(this.state.size).keys()).map((value) => (
           <Counter key={value} />
         ))}
