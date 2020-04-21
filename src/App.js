bafe1e0714d4c91a345236e72ba920d24bc83a54
Marco Lam <lamma3@oocl.com>
2020-04-21 16:25:08 +0800

    add CounterGroup component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React from "react";
 import "./App.css";
-import Counter from "./components/Counter";
+import CounterGroup from "./components/CounterGroup";
 
 function App() {
   return (
     <div className="App">
-      <Counter />
+      <CounterGroup />
     </div>
   );
 }
