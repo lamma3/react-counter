99114a6a2fe2be134c314f12c5125cf041c17f8b
Marco Lam <lamma3@oocl.com>
2020-04-21 16:30:31 +0800

    add Counter in CounterGroup

--- src/components/CounterGroup.js
+++ src/components/CounterGroup.js
@@ -1,13 +1,24 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
+import Counter from "./Counter";
 
 export class CounterGroup extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      size: 2,
+    };
+  }
+
   render() {
     return (
       <div>
-        
+        {Array.from(Array(this.state.size).keys()).map((value) => (
+          <Counter key={value} />
+        ))}
       </div>
-    )
+    );
   }
 }
 
-export default CounterGroup
+export default CounterGroup;
