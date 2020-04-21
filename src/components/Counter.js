01d9911522146243a0098fe25368f6be34c4e595
Marco Lam <lamma3@oocl.com>
2020-04-21 14:56:49 +0800

    show number in counter component

---
+++ src/components/Counter.js
@@ -0,0 +1,23 @@
+import React, { Component } from "react";
+
+export class Counter extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      number: 0,
+    };
+  }
+
+  render() {
+    return (
+      <div>
+        <button>+</button>
+        <span>{this.state.number}</span>
+        <button>-</button>
+      </div>
+    );
+  }
+}
+
+export default Counter;
